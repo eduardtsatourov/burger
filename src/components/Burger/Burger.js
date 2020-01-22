262bae2f08293e6162030fe5b3a30b548eb7dc30
eduard.tsatourov <tsatourov.eduard@ordina.be>
2020-01-22 12:55:04 +0100

    added dynamic ingredient injection from state in burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+  let outPut = null;
+  let ing = Object.keys(props.ingredients);
+
+  console.log(props.ingredients)
+
+  outPut = ing.map((ingName) => {
+    return [...Array(props.ingredients[ingName])].map((_, i) => {
+      return <BurgerIngredient key={ingName + i} type={ingName}/>
+    })
+  })
+  console.log(outPut)
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="bacon"/>
-      <BurgerIngredient type="salad"/>
+      {outPut}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   )
