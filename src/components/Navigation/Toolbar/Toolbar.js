12142f44c87c3f9ac66212e8e3c94e380c4b995d
eduard.tsatourov <tsatourov.eduard@ordina.be>
2020-01-23 11:54:33 +0100

    added responsiveness to nav elements

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const toolbar = props => {
       <div className={classes.Logo}>
         <Logo />
       </div>
-      <nav>
+      <nav className={classes.hideNav}>
         <NavigationItems />
       </nav>
     </header>
