82b3940c79077400f5276094b6b750794015624f
eduard.tsatourov <tsatourov.eduard@ordina.be>
2020-01-27 13:48:08 +0100

    added contact form

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,34 +1,41 @@
-import React, { Component } from 'react';
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import classes from './Checkout.module.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
+import classes from "./Checkout.module.css";
 
 class Checkout extends Component {
-  state={
+  state = {
     ingredients: {
       salad: 1,
       meat: 1,
       bacon: 1,
-      cheese:1
+      cheese: 1
     }
-  }
+  };
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
-  }
+  };
 
   checkoutContinuedHandler = () => {
-    this.props.history.replace('./checkout/contact-data');
-  }
+    this.props.history.replace("./checkout/contact-data");
+  };
 
   render() {
     return (
       <div className={classes.Checkout}>
-        <CheckoutSummary 
+        <CheckoutSummary
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler} />
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
+        />
       </div>
-    )
+    );
   }
 }
 
